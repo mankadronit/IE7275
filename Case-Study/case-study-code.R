72a2ae58b0f76073ad48c4b3284e3cf2acdbf357
Ronit Mankad <mankadronit.rm@gmail.com>
2020-05-24 11:46:30 -0400

    Add Case Study proposal .docx file

--- Case-Study/case-study-code.R
+++ Case-Study/case-study-code.R
@@ -1,4 +1,13 @@
 # Case Study
-library(dplyr)
+library(tidyverse)
+library(psych)
 
 df <- read.csv("./data/phishing_websites.csv")
+
+df <- df[, !colnames(df) %in% c("HttpsInHostname")]
+
+fa.parallel(scale(df[, 1:47]), fa = "pc", n.iter = 100, show.legend = FALSE)
+
+pc <- principal(scale(df[, 1:47]), nfactors = 13, rotate = "none", scores = TRUE)
+
+biplot.psych(pc, choose = c(1, 2), labels = df$CLASS_LABEL)
